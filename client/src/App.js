2b2ced26cbaedcce6d7299f25d1a1d4aec24ed5d
TG Kim <tg.kim@weballin.com>
2021-12-28 16:13:03 +0900

    useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log('once');
+  }, []);
+
+  useEffect(() => {
+    console.log('keyword is', keyword);
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
+      <h1 className={styles.title}>Welcome back!!! : {value}</h1>
+      <input type="text" placeholder="input here" onChange={onChange} value={keyword} />
+      <button onClick={onClick}>Click Me!</button>
       <Button text={"hello"} />
     </div>
   );
